9cc95ce9c1bb4ef2b8f78b44c5548e0494503b47
Ivan <wailintun@gusto.edu.mm>
2024-03-20 09:44:14 +0630

    Blog Build #1

---
+++ blog/resources/views/layouts/essentials/header.blade.php
@@ -0,0 +1,59 @@
+{{-- Header --}}
+<header class="flex items-center justify-between py-3 px-6 border-b border-gray-100">
+    <div id="header-left" class="flex items-center">
+        <div class="text-gray-800 font-semibold">
+            <span class="text-blue-900 text-xl">&lt;Wai Lin's Blog&gt;</span>
+        </div>
+        <div class="top-menu ml-10">
+            <ul class="flex space-x-4">
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-blue-900 text-sm text-blue-500"
+                        href="http://127.0.0.1:8000">
+                        Home
+                    </a>
+                </li>
+
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-blue-500 text-sm text-gray-500"
+                        href="http://127.0.0.1:8000/blog">
+                        Blog
+                    </a>
+                </li>
+
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-blue-500 text-sm text-gray-500"
+                        href="http://127.0.0.1:8000/blog">
+                        About Us
+                    </a>
+                </li>
+
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-blue-500 text-sm text-gray-500"
+                        href="http://127.0.0.1:8000/blog">
+                        Contact Us
+                    </a>
+                </li>
+
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-blue-500 text-sm text-gray-500"
+                        href="http://127.0.0.1:8000/blog">
+                        Terms
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+    <div id="header-right" class="flex items-center md:space-x-6">
+        <div class="flex space-x-5">
+            <a class="flex space-x-2 items-center hover:text-blue-500 text-sm text-gray-500"
+                href="http://127.0.0.1:8000/login">
+                Login
+            </a>
+            <a class="flex space-x-2 items-center hover:text-blue-500 text-sm text-gray-500"
+                href="http://127.0.0.1:8000/register">
+                Register
+            </a>
+        </div>
+    </div>
+</header>
