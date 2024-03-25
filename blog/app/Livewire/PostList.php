d9e4f33ad21d2030c075f0ee47a1763990d4d6e8
Ivan <wailintun@gusto.edu.mm>
2024-03-25 14:46:05 +0630

    update #2.1

--- blog/app/Livewire/PostList.php
+++ blog/app/Livewire/PostList.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Post;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -13,13 +14,21 @@ class PostList extends Component
     use WithPagination;
     #[Url()]
     public $sort = 'desc';
-    public function setSort($sort) {
+    #[Url()]
+    public $search = '';
+    public function setSort($sort)
+    {
         $this->sort = ($sort === 'desc') ? 'desc' : 'asc';
+    }
+    #[On('search')]
+    public function updateSearch($search)
+    {
+        $this->search = $search;
     }
     #[Computed()]
     public function posts()
     {
-        return Post::published()->orderBy('publish_at', $this->sort)->paginate(3);
+        return Post::published()->orderBy('publish_at', $this->sort)->where('title', 'like', "%$this->search%")->paginate(3);
     }
     public function render()
     {
--- blog/app/Livewire/SearchBox.php
+++ blog/app/Livewire/SearchBox.php
@@ -1,11 +1,19 @@
+<!-- This feature is no longer used -->
 <?php
 
 namespace App\Livewire;
 
 use Livewire\Component;
 
+use function Laravel\Prompts\search;
+
 class SearchBox extends Component
 {
+    public $search = '';
+    public function updateSearch()
+    {
+        $this->dispatch('search', search: $this->search);
+    }
     public function render()
     {
         return view('livewire.search-box');
